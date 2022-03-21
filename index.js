4a644da770f9991bd7dbf79d5a83b3c18a72265b
lea marsela <lea.marsela@gmail.com>
2022-03-21 15:30:56 +0700

    add allow headers

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function getData() {
       'Access-Control-Allow-Origin': '*',
       "Access-Control-Allow-Credentials" : true,
       'Content-Type': 'application/json',
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
     }
   });
 
